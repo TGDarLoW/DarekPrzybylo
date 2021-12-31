5b5601b5a1171fdcf2af6e277ed8fe54764ac7cd
Darek <darlow.the.master@gmail.com>
2021-12-31 14:17:37 +0100

    commit ostatecznej strony

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
     res.send("<h1>Moja aplikacja - Darek Przybylo 3i2b </h1>")
 })
 app.get("/data", function (req, res) {
-    res.send(JSON.stringify({ imie: "Darek" }, { nazwisko: "Przybylo" }, { klasa: "3i2" }, { grupa: "b" }))
+    res.send(JSON.stringify({ imie: "Darek", nazwisko: "Przybylo", klasa: "3i2", grupa: "b" }))
 })
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
