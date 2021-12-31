00a3e2f83b2c67a1efe2cb56f94959467d12f092
Darek <darlow.the.master@gmail.com>
2021-12-31 14:14:26 +0100

    commit ostatecznej aplikacji

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku - kolejna zmiana w pliku!</h1>")
+    res.send("<h1>Moja aplikacja - Darek Przybylo 3i2b </h1>")
 })
-
+app.get("/data", function (req, res) {
+    res.send(JSON.stringify({ imie: "Darek" }, { nazwisko: "Przybylo" }, { klasa: "3i2" }, { grupa: "b" }))
+})
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
